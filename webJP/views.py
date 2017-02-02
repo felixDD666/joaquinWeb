d7c90380a254c0e18ce370533fb1190f08b0a06a
FelixDD <t@ejemplo.com>
2017-02-02 19:54:46 +0100

    commit 02-02-2017

--- webJP/views.py
+++ webJP/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 def principal(request):
@@ -10,14 +11,47 @@ def CVJ(request):
 def CVP(request):
 	return render(request,'webJP/CV-pilar.html',{})
 
+def CVJU(request):
+	return render(request,'webJP/CV-juan.html',{})
+
+def CVR(request):
+	return render(request,'webJP/CV-ramona.html',{})
+
 def serv1(request):
-	return render(request,'webJP/podoDeportiva.html',{})
+	return render(request,'webJP/quiro.html',{})
 
 def serv2(request):
-	return render(request,'webJP/ciruPodo.html',{})
+	return render(request,'webJP/podoDeportiva.html',{})
 
 def serv3(request):
-	return render(request,'webJP/teraLaser.html',{})
+	return render(request,'webJP/plantillas.html',{})
 
 def serv4(request):
+	return render(request,'webJP/ciruPodo.html',{})
+
+def serv5(request):
+	return render(request,'webJP/teraLaser.html',{})
+
+def serv6(request):
+	return render(request,'webJP/podoInf.html',{})
+
+def serv7(request):
+	return render(request,'webJP/pieDiabe.html',{})
+
+def serv8(request):
 	return render(request,'webJP/plasma.html',{})
+
+def serv9(request):
+	return render(request,'webJP/eco.html',{})
+
+def serv10(request):
+	return render(request,'webJP/posturo.html',{})
+
+def serv11(request):
+	return render(request,'webJP/opto.html',{})
+
+def serv12(request):
+	return render(request,'webJP/recursos.html',{})
+def contacto(request):
+	form = ContactForm(request.POST)
+	return render(request,'webJP/contactView.html',{'form': form})
